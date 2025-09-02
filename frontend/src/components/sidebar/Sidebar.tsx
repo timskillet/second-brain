4097bd9e0a637ff5bfd00f24ea519a579d2b44ac
Tim Choy <timmymc02@gmail.com>
2025-09-01 19:38:43 -0700

    Update sidebar component

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -22,36 +22,13 @@ interface SidebarProps {
   onRootDirectoryChange: (newDirectory: string) => void;
 }
 
-const dummyChats: Chat[] = [
-  { id: "1", name: "Chat 1" },
-  { id: "2", name: "Chat 2" },
-  { id: "3", name: "Chat 3" },
-  { id: "4", name: "Chat 4" },
-  { id: "5", name: "Chat 5" },
-  { id: "6", name: "Chat 6" },
-  { id: "7", name: "Chat 7" },
-  { id: "8", name: "Chat 8" },
-  { id: "9", name: "Chat 9" },
-  { id: "10", name: "Chat 10" },
-  { id: "11", name: "Chat 11" },
-  { id: "12", name: "Chat 12" },
-  { id: "13", name: "Chat 13" },
-  { id: "14", name: "Chat 14" },
-  { id: "15", name: "Chat 15" },
-  { id: "16", name: "Chat 16" },
-  { id: "17", name: "Chat 17" },
-  { id: "18", name: "Chat 18" },
-  { id: "19", name: "Chat 19" },
-  { id: "20", name: "Chat 20" },
-];
-
 const Sidebar = ({
   fileData,
   rootDirectory,
   onRootDirectoryChange,
 }: SidebarProps) => {
   const [open, setOpen] = useState(true);
-  const [chats, setChats] = useState<Chat[]>(dummyChats);
+  const [chats, setChats] = useState<Chat[]>();
   const [fileHovering, setFileHovering] = useState(false);
   const [uploadFile, setUploadFile] = useState<File | null>(null);
   const [selectedFile, setSelectedFile] = useState<FileNode | null>(null);
